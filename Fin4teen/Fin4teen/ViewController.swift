50200cbeb8bd2ab6a31d48a93f1724ca36ac2bfc
leojportes <leojportes@gmail.com>
2021-05-03 18:18:16 -0300

    Login, Cadastro, Recupera senha c/ Firebase Ok

---
+++ Fin4teen/Fin4teen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fin4teen
+//
+//  Created by Leonardo Oliveira Portes on 03/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fin4teen/Pods/Target Support Files/Pods-Fin4teen/Pods-Fin4teen-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fin4teen : NSObject
+@end
+@implementation PodsDummy_Pods_Fin4teen
+@end
